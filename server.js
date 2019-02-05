e6e9bb6bc08c300beb0e4bbbc934ddee176a6f3f
Brad Rumack <brad.rumack@gmail.com>
2019-02-04 19:55:40 -0800

    added static folder code to serve up react front end

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const path = require('path')
 const apiRoutes = require('./routes/apiRoutes')
 
 app.use('/api', apiRoutes)
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
