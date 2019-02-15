389a3a20a0ea5e5b8944fd1922695f7f09d307b6
Brad Rumack <brad.rumack@gmail.com>
2019-02-14 19:19:04 -0800

    removed console.logs

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -34,16 +34,10 @@ export default class Dashboard extends React.Component {
     const { userAssets } = newProps
     const { assetValues } = this.state
 
-    console.log('pre userassets', userAssets)
-    console.log('pre assetValues', assetValues)
-
     Object.keys(assetValues).map(asset => {
-      console.log(userAssets)
       if (userAssets.map(userAsset => userAsset.asset).indexOf(asset) === -1)
         assetValues[asset] = 0
     })
-
-    console.log('post assetValues', assetValues)
 
     this.calculateTotalValue(assetValues)
     this.setState({ assetValues })
@@ -57,7 +51,6 @@ export default class Dashboard extends React.Component {
   handleReturnedValues = asset => {
     const { assetValues } = this.state
     assetValues[asset.name] = asset.value
-    console.log('from handle', assetValues)
     this.calculateTotalValue(assetValues)
     this.setState({ assetValues })
   }
@@ -69,7 +62,6 @@ export default class Dashboard extends React.Component {
   calculateTotalValue(assetValues) {
     if (Object.keys(assetValues).length > 0) {
       const portfolioValue = Object.values(assetValues).reduce((a, b) => a + b).toFixed(2)
-      console.log(portfolioValue)
       this.setState({ portfolioValue })
     }
   }
